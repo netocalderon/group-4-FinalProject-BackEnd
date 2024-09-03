1fa4f100ee7955e63152bbcd7fee7b0456edbfa4
jgchoti <jgchotirat@gmail.com>
2024-09-04 00:58:11 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,9 @@ import userControllers from '../controllers/user.js';
 
 const router = express.Router();
 
+router.post('/register', userControllers.register);
+router.post('/login', userControllers.login);
+router.post('/logout', userControllers.logout);
 
-// routes
 
 export default router;
