78db06dd1745358c6b3858e015f7a737591f7556
jgchoti <jgchotirat@gmail.com>
2024-09-07 00:43:04 +0200

    Update book.js

--- routes/book.js
+++ routes/book.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import verifyToken from '../middleware/verifyToken.js'; // import verifyToken from '../middleware/verifyToken.js';
 import bookControllers from '../controllers/book.js';
 
 const router = express.Router();
 
 router.get('/all', bookControllers.getAllBooks);
-
+router.get('/recent', bookControllers.getRecentBooks);
 router.get('/best', bookControllers.getAllBestSellerBooks);
+
+router.post('/add', verifyToken, bookControllers.addBook);
+// router.post('/edit', verifyToken, bookControllers.editBook);
+// router.post('/delete', verifyToken, bookControllers.deleteBook);
 
 export default router;
