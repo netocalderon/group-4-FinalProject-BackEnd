95cf0ec5b2ffeb98c77b84f06f82a658555e3608
jgchoti <jgchotirat@gmail.com>
2024-09-07 00:42:52 +0200

    Update verifyToken.js

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,12 +1,20 @@
 import jwt from 'jsonwebtoken';
 
 const verifyToken = (req, res, next) => {
-    const token = req.cookies.token;
+    const authHeader = req.headers['authorization'];
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Authorization token is missing or invalid' });
+    }
+
+    // Extract the token from the Authorization header
+    const token = authHeader.split(' ')[1];
+    console.log("token", token)
     if (token) {
         jwt.verify(token, process.env.TOKEN_ACCESS_SECRET, (err, data) => {
             if (err) {
                 res.status(498).json({ message: 'token is not valid' });
             }
+            req.user = data;
             next();
         });
     } else {
