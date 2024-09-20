75ac567aa7e633280152fba62372e5c47a11f23a
jgchoti <jgchotirat@gmail.com>
2024-09-20 20:22:22 +0200

    Update book.js

--- routes/book.js
+++ routes/book.js
@@ -8,6 +8,7 @@ router.get('/all', bookControllers.getAllBooks);
 router.get('/recent', bookControllers.getRecentBooks);
 router.get('/best', bookControllers.getBestSellerBooks);
 router.get('/:id', bookControllers.getBookById);
+router.get('/search/:q', bookControllers.getBookByQuery);
 router.post('/add', verifyToken, bookControllers.addBook);
 router.put('/:id', verifyToken, bookControllers.editBook);
 router.delete('/:id', verifyToken, bookControllers.deleteBook);
