dc83ddf2b2f268fa9939dda81c53ed32b0080b8f
jgchoti <jgchotirat@gmail.com>
2024-09-27 11:25:46 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,19 +5,19 @@ const createUserTable = async () => {
     const sql = `
         CREATE TABLE IF NOT EXISTS users (
             id INT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL,
+            username VARCHAR(191) NOT NULL,
+            email VARCHAR(191) NOT NULL UNIQUE,
             password VARCHAR(255) NOT NULL,
             phonenumber VARCHAR(50),
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        )
-`;
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+    `;
 
     try {
-        await query(sql)
-        console.log('users table created successfully');
-        // create test user
+        await query(sql);
+        console.log('Users table created successfully');
+
+        // Create test user
         const checkUserSql = `SELECT * FROM users WHERE email = ?`;
         const testUserEmail = "testuser@example.com";
         const users = await query(checkUserSql, [testUserEmail]);
@@ -34,7 +34,6 @@ const createUserTable = async () => {
             await query(insertUserSql, [username, testUserEmail, password, phonenumber]);
 
             console.log('TestUser created successfully');
-
         } else {
             console.log("Test user already exists");
         }
